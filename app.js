5f592ba58290097b2efaf3adedea0a74cbe2084c
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-11-09 07:56:14 -0500

    added middleware - lecture 59 complete

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const file = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
 let tours;
 if (file) {
@@ -13,6 +24,7 @@ if (file) {
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours ? tours.length : 0,
     data: {
       tours
