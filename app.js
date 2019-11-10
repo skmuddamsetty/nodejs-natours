ebfba2ebf088f384c097718377cbb00d531cda18
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-11-10 09:39:00 -0500

    comlpeted lecture 63

--- app.js
+++ app.js
@@ -30,7 +30,5 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 // ROUTES END
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+
+module.exports = app;
