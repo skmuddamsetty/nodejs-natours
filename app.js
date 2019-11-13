9d94a0269ba3f636fc1b35300eac792f8977e16d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-11-13 06:43:29 -0500

    section 6 complete

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARES START
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
